9176b23e17861b69509ba27db0fb6e7b3f5f633e
Crystal Lor <crystalalor@gmail.com>
2023-12-06 00:22:35 +0000

    An example of creating a reference error by attempting to invoke a local variable outside of scope.

--- Functions/greeting.js
+++ Functions/greeting.js
@@ -1,12 +1,7 @@
-let greetingMessage = "Good Morning!";
-
 function greetPeople() {
+  let greetingMessage = "Good Morning!";
   console.log(greetingMessage);
 }
 
-function changeGreetingMessage(newMessage) {
-  greetingMessage = newMessage;
-}
-
-changeGreetingMessage("Good Evening");
-greetPeople(); 
+greetPeople();
+console.log(greetingMessage);
