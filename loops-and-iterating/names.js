3c858944a831d856104728be7168116435130c57
Crystal Lor <crystalalor@gmail.com>
2023-12-09 08:39:48 +0000

    A negated if conditional can act like continue.

--- loops-and-iterating/names.js
+++ loops-and-iterating/names.js
@@ -2,12 +2,10 @@ let names = ['Chris', 'Kevin', 'Naveed', 'Pete', 'Victor'];
 let upperNames = [];
 
 for (let index = 0; index < names.length; index += 1) {
-  if (names[index] === 'Naveed') {
-    continue;
-  }
-  
+  if (names[index] !== 'Naveed') {
   let upperCaseName = names[index].toUpperCase();
   upperNames.push(upperCaseName);
+  }
 }
 
 console.log(upperNames);
