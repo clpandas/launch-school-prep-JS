dea5e048598856d550915ce6022d948f4faa75d9
Crystal Lor <crystalalor@gmail.com>
2023-12-09 08:20:16 +0000

    Looping over an array with a for loop, which demonstrates the same functionality as a while loop, but with syntactical differences.

--- loops-and-iterating/names.js
+++ loops-and-iterating/names.js
@@ -1,11 +1,9 @@
 let names = ['Chris', 'Kevin', 'Naveed', 'Pete', 'Victor'];
 let upperNames = [];
-let index = 0;
 
-while (index < names.length) {
+for (let index = 0; index < names.length; index += 1) {
   let upperCaseName = names[index].toUpperCase();
   upperNames.push(upperCaseName);
-  index += 1;
 }
 
 console.log(upperNames);
