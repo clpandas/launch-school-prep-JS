bfa70fc70be000417e4f2fef9d7401dbc4a44643
Crystal Lor <crystalalor@gmail.com>
2023-12-10 08:31:57 +0000

    This is a function that combines the built-in map and filter functions to first determine the numeric lengths of all the elements in an array of strings values, and then discard the even values, keeping the odd values.

---
+++ arrays/oddLengths.js
@@ -0,0 +1,8 @@
+let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+console.log(oddLengths(arr));
+
+function oddLengths(strings) {
+  let lengths = strings.map((letters) => letters.length);
+  let oddLengths = lengths.filter((number) => number % 2 === 1);
+  return oddLengths;
+}
