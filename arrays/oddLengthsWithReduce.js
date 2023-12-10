f73c3fd70301131f4d355266da9bc43a4c2e229f
Crystal Lor <crystalalor@gmail.com>
2023-12-10 08:55:54 +0000

    This function uses the reduce method to determine the lengths of all the elements in an array of string values, and then discard the even values (keeping the odd values). This was a tough one. Also typed copy paste.  Will have to revisit. Emphasis, this exercise demonstrates how you can use the reduce method to mitigate some of the expensive computational iterations you have when combining methods like map and filter.

---
+++ arrays/oddLengthsWithReduce.js
@@ -0,0 +1,13 @@
+let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+console.log(oddLengths(arr));
+
+function oddLengths(strings) {
+  return strings.reduce((filteredNumbersArray, letters) => {
+    let length = letters.length;
+    if (length % 2 === 1) {
+      filteredNumbersArray.push(length);
+    }
+    
+    return filteredNumbersArray;
+  }, []);
+}
