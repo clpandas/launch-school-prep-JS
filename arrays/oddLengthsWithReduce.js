dcbf3c9a8cf45fc61a8481f59e1bcecdfbc97330
Crystal Lor <crystalalor@gmail.com>
2023-12-10 09:31:49 +0000

    Practice using the includes method to determine whether a given element (3) is in the calling array, and returning true if it is and false if it is not.

--- arrays/oddLengthsWithReduce.js
+++ arrays/oddLengthsWithReduce.js
@@ -5,6 +5,7 @@ function oddLengths(strings) {
   return strings.reduce((filteredNumbersArray, letters) => {
     let length = letters.length;
     if (length % 2 === 1) {
+      
       filteredNumbersArray.push(length);
     }
     
