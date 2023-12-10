0ca18b15e501c097e4e300e83c8f874037cf8120
Crystal Lor <crystalalor@gmail.com>
2023-12-10 07:33:01 +0000

    A forEach implementation of checking whether each of the elements in the array is even.

--- arrays/evenElements.js
+++ arrays/evenElements.js
@@ -1,9 +1,8 @@
 let myArray = [1, 3, 6, 11, 4, 2,
                4, 9, 17, 16, 0];
 
-for (let i = 0; 1 <myArray.length; i += 1) {
-  let value = myArray[i];
+myArray.forEach(function(value) {
   if (value % 2 === 0) {
     console.log(value);
   }
-}
+});
