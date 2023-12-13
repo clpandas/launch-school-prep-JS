678eac8912739877386962d0b1c4058c7ddbd4fd
Crystal Lor <crystalalor@gmail.com>
2023-12-13 15:54:41 +0000

    An alternative function utilizing the filter and test methods to return all matching elements in an array.

--- more-stuff/regex.js
+++ more-stuff/regex.js
@@ -8,14 +8,7 @@ let words = [
 ];
 
 function allMatches(words, pattern) {
-  let matches = [];
-  for (let index = 0; index < words.length; index += 1) {
-    if (pattern.test(words[index])) {
-      matches.push(words[index]);
-    }
-  }
-
-  return matches;
+  return words.filter((word) => pattern.test(word));
 }
 
 console.log(allMatches(words, /lab/));
