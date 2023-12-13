02e0fff1bc66cc8f780909159b513657e4a38246
Crystal Lor <crystalalor@gmail.com>
2023-12-13 05:26:50 +0000

    Implementing the ?. operator to perform optional chaining, by evaluating to undefined if the sentence is nullish.

--- more-stuff/optional_chaining.js
+++ more-stuff/optional_chaining.js
@@ -1,7 +1,9 @@
 function reverse_words(sentence) {
-  return sentence.split(' ')
-                 .reverse()
-                 .join(' ');
+  return sentence?.split(' ')
+                  .reverse()
+                  .join(' ');
 }
 
 console.log(reverse_words("Four score and seven"));
+console.log(reverse_words(null));
+console.log(reverse_words(undefined));
