81fbd9c605ad6edba132223050f927995f69e62b
Crystal Lor <crystalalor@gmail.com>
2023-12-16 03:18:12 +0000

    Additional comments highlighting the significance of specific details in the while loop.

--- js-basics-exercises/loops/looping_over_array_elements.js
+++ js-basics-exercises/loops/looping_over_array_elements.js
@@ -5,3 +5,15 @@ while (index < array.length) {
   console.log(array[index]);
   index += 1;
 }
+
+/*
+Line 5 lets us access the array element at the current index, 
+and log the returned element to the console with console.log. 
+Then, we increment index by 1.
+
+After each iteration of the loop, the while condition is evaluated. 
+Because JavaScript arrays have a zero-based index, 
+we want to terminate the loop when index is equal to array.length,
+because the last index of an array is always one less than the array's length. 
+
+*/
