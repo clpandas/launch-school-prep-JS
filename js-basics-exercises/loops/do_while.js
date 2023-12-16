20b20d9827e3f27258d6ae08609501447f57550f
Crystal Lor <crystalalor@gmail.com>
2023-12-16 04:16:11 +0000

    Addtional comments to clarify the output of each loop.

--- js-basics-exercises/loops/do_while.js
+++ js-basics-exercises/loops/do_while.js
@@ -4,8 +4,11 @@ while (counter > 0) {
   console.log('Woooot!');
   counter -= 1;
 }
+// This will not output anything because the condition is not satisfied. 
+
 
 do {
   console.log('Woooot!');
   counter -= 1;
 } while (counter > 0);
+// This will output 'Woooot!' a single time because a do...while statement always executes at least once because the statement is checked after the block has been executed. 
